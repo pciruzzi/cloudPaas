30a9292875da363d5d6f5d46d3d32e760eb5af37
Pipi <p.ciruzzi@hotmail.com>
2017-01-09 23:49:11 +0100

    Container creation via Rancher API seems to work!

--- rancherServer.py
+++ rancherServer.py
@@ -1,15 +1,35 @@
 import requests
 import json
 import sys
+import subprocess
 
 global BASE 
-BASE = "http://213.32.27.235:8083/v1/"
-global HOSTS
-HOSTS = BASE + "hosts"
+BASE = "http://213.32.27.235:8083/"
+global V1
+V1 = BASE + "v1/"
+global V2_BETA
+V2_BETA = BASE + "v2-beta/"
 
-username = sys.argv[1]
-password = sys.argv[2]
-response = requests.get(HOSTS, auth=(username, password))
+global HOSTS
+HOSTS = V1 + "hosts"
+global CONTAINERS
+CONTAINERS = V2_BETA + "projects/1a5/containers"
+
+
+rancherUser = sys.argv[1]
+rancherPassword = sys.argv[2]
+response = requests.get(HOSTS, auth=(rancherUser, rancherPassword))
 jsonResponse = response.json()
 print response.status_code
 #print json.dumps(jsonResponse, indent=2)
+
+print "##############################################"
+data = '{"description":"Une description", "imageUuid":"docker:pciruzzi/paasinsa1617", "name":"api-test", "ports":["8092:3000/tcp"]}'
+# response = requests.post(CONTAINERS, auth=(rancherUser, rancherPassword), data=data)
+# jsonResponse = response.json()
+# print response.status_code
+
+# TODO: Obtain in what VM was it created...
+print "##############################################"
+#subprocess.Popen("iptables -t nat -A PREROUTING -p tcp --dport 8092 -j DNAT --to-destination 192.168.0.3:8092")
+subprocess.Popen("ls")
